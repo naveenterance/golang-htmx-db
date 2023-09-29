35e6459f18a6ef9a888610a679be0d5d681937d3
naveenterance <naveenterance@gmail.com>
2023-09-29 07:37:41 +0530

    view+add

--- main.go
+++ main.go
@@ -1,49 +1,91 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
+	"text/template"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
 )
 
-type Film struct {
-	Title    string
-	Director string
+var db *sql.DB
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
 }
 
 func main() {
-	fmt.Println("Go app...")
 
-	// handler function #1 - returns the index.html template, with film data
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := map[string][]Film{
-			"Films": {
-				{Title: "The Godfather", Director: "Francis Ford Coppola"},
-				{Title: "Blade Runner", Director: "Ridley Scott"},
-				{Title: "The Thing", Director: "John Carpenter"},
-			},
-		}
-		tmpl.Execute(w, films)
+	cfg := mysql.Config{
+		User:                 "root",
+		Passwd:               "nst",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
 	}
 
-	// handler function #2 - returns the template block with the newly added film, as an HTMX response
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		var albums []Album
+
+		rows, err := db.Query("SELECT * FROM album")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+			var alb Album
+			if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+				fmt.Printf("error")
+			}
+
+			albums = append(albums, alb)
+		}
+
+		tmpl.Execute(w, albums)
+
+	}
+
 	h2 := func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
 		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
-		// htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
-		// tmpl, _ := template.New("t").Parse(htmlStr)
+		artist := r.PostFormValue("artist")
+		price := r.PostFormValue("price")
+		_, err = db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", title, artist, price)
+
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
+		tmpl.ExecuteTemplate(w, "film-list-element", Album{Title: title, Artist: artist, Price: price})
 	}
 
-	// define handlers
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/add-film/", h2)
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
